f0286ef3c6e74e01da10dbb7a00ca719ae9b4b6b
milomacphail <milomacphail@gmail.com>
2019-10-26 16:53:44 -0400

    Automated validation implemented

--- index.js
+++ index.js
@@ -36,6 +36,23 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 let demoChain = new Blockchain();
@@ -51,3 +68,4 @@ demoChain.addBlock(
 );
 
 console.log(JSON.stringify(demoChain, null, 4));
+console.log("Is chain valid " + demoChain.isChainValid());
