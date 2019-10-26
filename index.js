3a9be95601d1ab2c427310ad36977f4b0cc13129
milomacphail <milomacphail@gmail.com>
2019-10-26 16:44:16 -0400

    Block chain is valid

---
+++ index.js
@@ -0,0 +1,53 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash) {
+    this.index = index;
+    this.timestamp = this.timestamp;
+    this.data = data;
+    this.previousHash = this.previousHash;
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "15/01/2019", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let demoChain = new Blockchain();
+demoChain.addBlock(
+  new Block(1, "15/02/2018", {
+    amount: 10
+  })
+);
+demoChain.addBlock(
+  new Block(2, "02/26/2018", {
+    amount: 25
+  })
+);
+
+console.log(JSON.stringify(demoChain, null, 4));
